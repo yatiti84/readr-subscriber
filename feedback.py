926664e87acea610d7e6a4c3413be6729e635efc
tingwei <yatiti84@gmail.com>
2022-05-19 16:57:50 +0800

    feat: add regex

--- feedback.py
+++ feedback.py
@@ -1,4 +1,5 @@
 import os
+import re
 from uuid import uuid4
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
@@ -12,7 +13,15 @@ def create_feedback(data):
   name = str(uuid4())
   form = data['form']
   ip = data['ip']
+  ip_regex = re.compile(r'[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+')
+  if not re.fullmatch(ip_regex, ip) :
+    print("ip format not match.")
+    return False
   responseTime = data['responseTime']
+  responseTime_regex = re.compile(r'20[0-9][0-9]-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|30|31)T([01][0-9]|2[0-3]):([0-5][0-9]|60):([0-5][0-9]|60).([0-9][0-9][0-9])Z')
+  if not re.fullmatch(responseTime_regex, responseTime) :
+    print("responseTime format not match.")
+    return False
   for result in data['result']:
     field = result['field']
     result = result['userFeedback']
@@ -55,10 +64,10 @@ def create_feedback(data):
   return True
 if __name__ == '__main__':
   data = {"form": "2",
-  "ip": "0.0.0.0",
+  "ip": "2.1.1.22",
   "responseTime": '2022-05-19T05:00:00.000Z',
   "result": [
       {"field": "6", "userFeedback": "不符合"},
       {"field": "7", "userFeedback": "巴拉巴拉使用者的實際經驗～～"},
 ]}
-  create_feedback(data)
+  print(create_feedback(data))
