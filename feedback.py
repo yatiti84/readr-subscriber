8821d2566ceb824a2e94e16387c5b75a7b6ff2db
tingwei <yatiti84@gmail.com>
2022-05-20 11:47:01 +0800

    feat: seperate comment and like data proccess

--- feedback.py
+++ feedback.py
@@ -10,76 +10,120 @@ comment_form_id = os.environ['COMMENT_FORM_ID']
 like_field_id = os.environ['LIKE_FIELD_ID']
 comment_field_id = os.environ['COMMENT_FIELD_ID']
 
+
+
 gql_transport = AIOHTTPTransport(url=gql_endpoint)
 gql_client = Client(transport=gql_transport, fetch_schema_from_transport=True)
-def create_feedback(data):
-  create_result = []
-  name = data['name']
-  form = data['form']
-  field = data['field']
-  result = data['userFeedback']
-  ip = data['ip']
-  ip_regex = re.compile(r'[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+')
-  if not re.fullmatch(ip_regex, ip) :
-    print("ip format not match.")
-    return False
-  responseTime = data['responseTime']
-  responseTime_regex = re.compile(r'20[0-9][0-9]-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|30|31)T([01][0-9]|2[0-3]):([0-5][0-9]|60):([0-5][0-9]|60).([0-9][0-9][0-9])Z')
-  if not re.fullmatch(responseTime_regex, responseTime) :
-    print("responseTime format not match.")
-    return False
-  
-
+def create_formResult(gql_client, name, ip, result, responseTime, form, field):
   mutation_data = '''
-      data: {
-        name: "%s",
-        ip: "%s",
-        result: "%s",
-        responseTime: "%s",
-        form: {
-          connect: {
-            id: "%s"
-          }
-        },
-        field: {
-          connect: {
-            id: "%s"
+        data: {
+          name: "%s",
+          ip: "%s",
+          result: "%s",
+          responseTime: "%s",
+          form: {
+            connect: {
+              id: "%s"
+            }
+          },
+          field: {
+            connect: {
+              id: "%s"
+            }
           }
         }
-      }
-    ''' %(name, ip, result, responseTime, form, field)
-  mutation = '''
+      ''' %(name, ip, result, responseTime, form, field)
+  createFormResult = '''
     mutation{
       createFormResult(%s){
         id
       }
     }
     ''' % mutation_data
-  print(mutation)
-  result = gql_client.execute(gql(mutation))
-  print(result)
-  if not isinstance(result, dict) and 'createFormResult' not in result:
-    print(result)
+  print(createFormResult)
+  mutation_result = gql_client.execute(gql(createFormResult))
+  if not isinstance(mutation_result, dict) and 'createFormResult' not in mutation_result:
+    print(mutation_result)
     return False
-  if isinstance(result['createFormResult'], dict) and 'id' in result['createFormResult']:
-    create_result.append(result['createFormResult']['id'])
+  if isinstance(mutation_result['createFormResult'], dict) and 'id' in mutation_result['createFormResult']:
+    return True
   else: 
-    print(result)
+    print(mutation_result)
     return False
-  return True
-
-  
-
-  
-if __name__ == '__main__':
-  data = {
-  "name": "uuid",
-  "form": "2",
-  "ip": "2.1.1.22",
-  "responseTime": '2022-05-19T05:00:00.000Z',
-  "field": "6",
-  "userFeedback": True,
+def delete_name_exist_result(gql_client, name):
+  query = '''query{
+    formResults(where:{name:{in:"%s"} ,field:{id:{in:%s}}} orderBy:{name:desc}){
+      id
+    }
   }
+''' %(name, like_field_id)
+  print(query)
+  query_result = gql_client.execute(gql(query))
+  if isinstance(query_result, dict) and 'formResults' in query_result:
+    if query_result['formResults']:
+      #the user's feed-like result in formResults
+      id = query_result['formResults'][0]['id']
+      deleteFormResult = '''
+      mutation{
+          deleteFormResult(where:{id:%s}){
+            id
+          }
+        }'''% id
+      print(deleteFormResult)
+      mutation_result = gql_client.execute(gql(deleteFormResult))
+      if not isinstance(mutation_result, dict) and 'deleteFormResult' not in mutation_result:
+        print(mutation_result)
+        print("deleteFormResult fail")
+        return False
+      if isinstance(mutation_result['deleteFormResult'], dict) and 'id' in mutation_result['deleteFormResult']:
+        return True
+      else: 
+        print(mutation_result)
+        print("deleteFormResult fail")
+        return False
+
+
+    else:
+      #user feed-like result not in formResults
+      return True
+      
+
+def feedback_handler(data):
+  name = data['name']
+  form = data['form']
+  field = data['field']
+  result = data['userFeedback'].lower()
+  ip = data['ip']
+  responseTime = data['responseTime']
+
+  ip_regex = re.compile(r'[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+')
+  if not re.fullmatch(ip_regex, ip) :
+    print("ip format not match.")
+    return False
+  responseTime_regex = re.compile(r'20[0-9][0-9]-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|30|31)T([01][0-9]|2[0-3]):([0-5][0-9]|60):([0-5][0-9]|60).([0-9][0-9][0-9])Z')
+  if not re.fullmatch(responseTime_regex, responseTime) :
+    print("responseTime format not match.")
+    return False
+
+  if (form == like_form_id and field == like_field_id) or (form == comment_form_id and field == comment_field_id):
+    if field == comment_field_id:
+      return create_formResult(gql_client, name, ip, result, responseTime, form, field)
+    elif field == like_field_id:
+      if delete_name_exist_result(gql_client, name) is False:
+        return False
+      if result == 'true' or result == 'false':
+        return create_formResult(gql_client, name, ip, result, responseTime, form, field) 
+      else: 
+        return True
+    else:
+      print("field not match.")
+      return False
+  else:
+    print("form and field not match.")
+    return False
+
+
+if __name__ == '__main__':
   data_comment = {
   "name": "uuid",
   "form": "3",
@@ -88,4 +132,4 @@ if __name__ == '__main__':
   "field": "7", 
   "userFeedback": "使用者的實際經驗感想",
   }
-  print(create_feedback(data))
+  print(feedback_handler(data_comment))
